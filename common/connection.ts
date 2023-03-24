9ef2b0274603942d6a763e8a4b020899c8ee9e7a
silaskenneth <silaskenneth1@gmail.com>
2023-03-24 20:37:05 +0300

    Make the database HOST configurable.

--- common/connection.ts
+++ common/connection.ts
@@ -6,6 +6,8 @@ import { logger } from "./logger";
 const sequelize = new Sequelize({
     isolationLevel: ISOLATION_LEVELS.READ_COMMITTED,
     dialect: "postgres",
+    host: dbConfig.DATABASE_HOST,
+    port: dbConfig.DATABASE_PORT,
     database: dbConfig.DATABASE_NAME,
     username: dbConfig.DATABASE_USERNAME,
     password: dbConfig.DATABASE_PASSWORD,
--- config/database.ts
+++ config/database.ts
@@ -6,6 +6,8 @@ const config = {
     DATABASE_USERNAME: process.env.DATABASE_USERNAME ?? "pguser",
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD ?? "Password@123",
     DATABASE_NAME: process.env.DATABASE_NAME ?? "employees",
+    DATABASE_HOST: process.env.DATABASE_HOST ?? 'localhost',
+    DATABASE_PORT: parseInt(process.env.DATABASE_PORT ?? '5432')
 };
 
 export { config as dbConfig };
